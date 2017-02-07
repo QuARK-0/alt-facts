78e57b0ce40cc91dfbb7dda28e5413a748e9d2ff
Andrew Maidah <a.maidah@gmail.com>
2017-02-07 15:15:52 -0800

    not sure

--- models/user.js
+++ models/user.js
@@ -5,12 +5,12 @@ var UserSchema = new mongoose.Schema({
   userName: String,
   f_name: String,
   l_name: String,
-  // gameHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
+  // gameHistory:
   // write a method that will query all game collection and return back game id's
   // instance and static mongoose methods
   gamesPlayed: Number,
-  totalScore: Number,
-  curScore: Number
+  totalScore: Number
+  // curScore: Number //hold into session then after game is finish, push at once
 })
 
 module.exports = mongoose.model('User', UserSchema);
