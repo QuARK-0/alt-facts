694af8935e9be4f32a17c50ed4fbb7d42ed0fc75
Ritwik Ruia <ritwikruia@gmail.com>
2017-02-08 11:09:44 -0800

    links db to socket via user google id

--- routes/game.js
+++ routes/game.js
@@ -8,8 +8,10 @@ router.get('/', (req, res, next) => {
   // console.log(user)
   // console.log('from game.js', req.session.user); //prints user from sessions
   // console.log('from game.js');
-  var name = req.session.user.displayName;
-  res.render('game', {data: name} );
+  // var name = req.session.user.displayName; // to send just the user name
+  // res.render('game', {data: name} );
+  var userGId = req.session.user.id; // user Google ID
+  res.render('game', {data: userGId} ); //sends Google ID to script tag in game.hbs
 });
 
 
