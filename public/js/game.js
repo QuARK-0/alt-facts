02c1b3fa2d5f66ca044bb2d839e2f32d4ed04060
Andrew Maidah <a.maidah@gmail.com>
2017-02-07 20:33:24 -0800

    fully merges all morning work with everthing else

--- public/js/game.js
+++ public/js/game.js
@@ -7,8 +7,47 @@ socket.emit('send-name', nameUser);
 
 socket.on('welcome-msg', welcome => {
   $('#welcome-msg').text(welcome);
+  $('#welcome-msg').after('<button id="ready-button">Ready?</button>')
 })
 
 socket.on('user-join', msg => {
   $('.connected-users').append(`<h3>${msg}</h3>`);
+})
+
+function readyHandle(evt) {
+  console.log('haha clicked');
+  $('body').off('click', '#ready-button', readyHandle);
+  $('#welcome-msg').text('waiting for other players to ready up...');
+  $('#ready-button').css('visibility', 'hidden');
+  socket.emit('ready', 1);
+}
+
+$('body').on('click', '#ready-button', readyHandle);
+
+socket.on('question', question => {
+  $('.connected-users').css('visibility', 'hidden');
+  $('#welcome-msg').text(question);
+  $('#welcome-msg').after('<input class="answer-input" type="text"><br><button class="answer-button">Submit Answer</button><br>');
+})
+
+function answerHandle(evt) {
+  var userAnswer = $('.answer-input').val();
+  console.log(userAnswer);
+  $('body').off('click', '.answer-button', answerHandle);
+  $('.answer-input').val('');
+  $('.answer-input').css('display', 'none');
+  $('.answer-button').css('visibility', 'hidden');
+  socket.emit('send-answer', userAnswer);
+}
+
+$('body').on('click', '.answer-button', answerHandle);
+
+socket.on('display-choices', obj => {
+  var keyNames = Object.keys(obj);
+  console.log(keyNames);
+  //input field still on the right side of displayed answers due
+  //to visibility hidden on line 38
+  for (var i = 0; i < keyNames.length; i++) {
+    $('#welcome-msg').after(`<button>${obj[keyNames[i]]}</button>`)
+  }
 })
--- socket.js
+++ socket.js
@@ -1,4 +1,5 @@
-
+var readyUsers = 0;
+var answerObj = {};
 
 //being required in server.js
 module.exports = function(io) {
@@ -13,22 +14,36 @@ module.exports = function(io) {
       // gets name from client-side script
       console.log('name from socket.js', name);
       socket.userName = name;
+
       //send message to user who just connected
       var welcomeUser = `Welcome ${socket.userName}`;
       socket.emit('welcome-msg', welcomeUser);
     })
 
-      console.log('socket.js outside', socket.userName);
-
-
-
-
+    console.log('socket.js outside', socket.userName);
 
     //sends message to anyone whos already connected
     var welcomeJoined = 'A New User has Joined';
     socket.broadcast.emit('user-join', welcomeJoined);
 
+    socket.on('ready', num => {
+      readyUsers += num;
+      console.log(readyUsers);
+      if (readyUsers === 2) {
+        var question = 'Know the meaning of fast?'
+        io.emit('question', question);
+      }
+    })
 
+    socket.on('send-answer', answer => {
+      answerObj[answer] = answer;
+      var trueAns = 'RIGHT ANSWER';
+      answerObj.trueAns = trueAns;
+      console.log(answerObj);
+      if (Object.keys(answerObj).length === 3) {
+        io.emit('display-choices', answerObj);
+      }
+    })
 
     socket.on('disconnect', () => {
       console.log('a user disconnected');
