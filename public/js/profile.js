0fba91deb2acd731f7e5227f6333ae4ec4af5436
Andrew Maidah <a.maidah@gmail.com>
2017-02-09 10:26:20 -0800

    adds profile page rendering and completes text to input back to text

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,18 +2,16 @@ console.log('hello from profile.js')
 
 var $updButton = $('.username-button');
 var $usernameText = $('.username-text');
+
 function updateUsername(evt) {
   console.log('clicked');
-
   $updButton.text('Save')
-
   var text = $usernameText.text()
+  //converts text span into input box
   $usernameText.html($(`<input class="input-user-box" style = "font-size: 1.5rem;" />`,{'value' : text}).val(text)); //size: ${$(text).length}
   $('.input-user-box').attr('size', text.length);
   $updButton.attr('class', 'save-username');
-
   $('body').on('click', '.save-username', saveNewUserName);
-
 }
 
 function saveNewUserName (evt) {
@@ -22,8 +20,10 @@ function saveNewUserName (evt) {
   $.post('/profile/me/update', {newUserName}, function (res) {
     console.log(res); //RETURNS STATUS 200
     if (res.status === 200) {
-      // convert back to text from input field - PENDING
-
+      // convert back to text from input field
+      $usernameText.html(`${newUserName}`)
+      $updButton.text('Update')
+      $updButton.attr('class', 'username-button');
     }
   })
 }
